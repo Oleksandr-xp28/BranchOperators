2cc37396903c39e634c79be5b5caff2641b91779
Fedor_xp28@student.itstep.org <Fedor_xp28@student.itstep.org>
2023-03-29 12:27:50 +0300

    Ex_1 task done

--- BranchOperators.py
+++ BranchOperators.py
@@ -1,4 +1,25 @@
+if __name__ == "__main__":
+    try:
+        num1 = float(input("Enter a number: "))
+        num2 = float(input("Enter a second number: "))
+        num3 = float(input("Enter a third number: "))
 
-# Homework Course: "Introduction to the language Python programming
-# Module 2: Branching operators, loops, exceptions
-# Topic: Branching operators. Part 1
+        print("What do you want to calculate?")
+        print("1. Sum of three numbers")
+        print("2. Product of three numbers")
+
+        choice = int(input("Enter your choice: "))
+
+        if choice == 1:
+            sum = num1 + num2 + num3
+            print("The sum of the three numbers is:", sum)
+        elif choice == 2:
+            product = num1 * num2 * num3
+            print("The product of the three numbers is:", product)
+        else:
+            print("Invalid choice. Please enter either 1 or 2.")
+        pass
+    except:
+        pass
+
+# Path: BranchOperators.py
